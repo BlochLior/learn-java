24eb1d59191f4a7952fb04e8cba535bd198da315
BlochLior <blochlior@gmail.com>
2025-08-20 18:21:34 +0300

    Fixed mistake in AssingmentOne.java

--- Recitations/1/AssignmentOne.java
+++ Recitations/1/AssignmentOne.java
@@ -1,11 +1,11 @@
 public class AssignmentOne {
 	public static void main(String[] args) {
-		int number = 100; 
+		int number = 10; 
         int modHundred = number % 100;
         if (modHundred == 0) {
             System.out.println("3 digits");
         } else {
-            if (number > 10) {
+            if (number > 9) {
                 System.out.println("2 digits");
             } else {
                 System.out.println("1 digit");
