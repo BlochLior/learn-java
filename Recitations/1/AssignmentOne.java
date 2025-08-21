ce8b415c2b0c8286e79f4c457c153b426366d790
BlochLior <blochlior@gmail.com>
2025-08-21 12:29:44 +0300

    fixed another error in AssignmentOne

--- Recitations/1/AssignmentOne.java
+++ Recitations/1/AssignmentOne.java
@@ -2,7 +2,7 @@ public class AssignmentOne {
 	public static void main(String[] args) {
 		int number = 10; 
         int modHundred = number % 100;
-        if (modHundred == 0) {
+        if (modHundred == 0 && number != 0) {
             System.out.println("3 digits");
         } else {
             if (number > 9) {
